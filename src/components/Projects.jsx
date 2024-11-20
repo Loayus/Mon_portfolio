4807194883da6eddbc7d5a80b48b97d75f7895f0
antoine_ledoux <antoine_ledoux@ens.univ-artois.fr>
2024-11-20 22:57:24 +0100

    correction fautes + renouvelement cv

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = () => {
                 transition={{ duration: 0.5 }}
                 className="my-20 text-center text-4xl"
             >
-                Projects
+                Projets
             </motion.h2>
             <div>
                 {PROJECTS.map((project, index) => (
