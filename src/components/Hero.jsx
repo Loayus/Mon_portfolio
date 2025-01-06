e3d9889a90c744a4a0c19785278f3eb439b389e4
Antoine Ledoux <antoineledoux@Antoines-MacBook.local>
2025-01-06 20:54:56 +0100

    :art: changement de couleur

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
 import {MapContainer, TileLayer, Circle, Popup, CircleMarker} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { HERO_CONTENT, HERO_CONTENT2 } from "../constants";
-import { motion } from "framer-motion";
+import {HERO_CONTENT, HERO_CONTENT2} from "../constants";
+import {motion} from "framer-motion";
 
 const container = (delay) => ({
-    hidden: { x: -100, opacity: 0 },
+    hidden: {x: -100, opacity: 0},
     visible: {
         x: 0,
         opacity: 1,
-        transition: { duration: 0.5, delay: delay },
+        transition: {duration: 0.5, delay: delay},
     },
 });
 
@@ -33,7 +33,7 @@ const Hero = () => {
                             variants={container(0.5)}
                             initial="hidden"
                             animate="visible"
-                            className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+                            className="bg-gradient-to-r from-yellow-800 via-orange-700 to-red-700 bg-clip-text text-4xl tracking-tight text-transparent"
                         >
                             Développeur Junior
                         </motion.span>
@@ -58,9 +58,9 @@ const Hero = () => {
                 <div className="w-full lg:w-1/2 lg:p-8 ">
                     <div className="flex justify-center pt-10">
                         <motion.div
-                            initial={{ x: 100, opacity: 0 }}
-                            animate={{ x: 0, opacity: 1 }}
-                            transition={{ duration: 1, delay: 1.2 }}
+                            initial={{x: 100, opacity: 0}}
+                            animate={{x: 0, opacity: 1}}
+                            transition={{duration: 1, delay: 1.2}}
                             className="w-full h-96 lg:h-[500px]" // Adjust the height here
                         >
                             <MapContainer
@@ -81,9 +81,9 @@ const Hero = () => {
                                 <Circle
                                     center={libercourtCoords}
                                     radius={radius}
-                                    pathOptions={{ color: 'red', fillColor: 'red', fillOpacity: 0.3 }}
+                                    pathOptions={{color: 'red', fillColor: 'red', fillOpacity: 0.3}}
                                 />
-                                <CircleMarker center={libercourtCoords} radius={40} pathOptions={{ color: 'black' }}>
+                                <CircleMarker center={libercourtCoords} radius={40} pathOptions={{color: 'black'}}>
                                     <Popup>Libercourt, France</Popup>
                                 </CircleMarker>
                             </MapContainer>
